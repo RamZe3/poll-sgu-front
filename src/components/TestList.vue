9940eaa79ab2d955655a4f0a2175b85c26d8e50c
Pavel <podoprigoraps@gmail.com>
2022-12-12 01:50:56 +0400

    styles , types of tests
    кнопк выйти +
    кнопк апройти на всю кнопка тесты +
    посмотреть резльтат на всю кнопку +
    удалить вариант ответа влево +
    отступы в создание теста +
    анимации вопросов побыстрее +
    количество балов за каждый ответ +
    добавить вес удалить вес +
    удалить вопрос светлый красный +
    ширина ввести + и ширина мои тесты ид ширина +
    модальное окно компонент по ссылке test для первой странички+-

--- src/components/TestList.vue
+++ src/components/TestList.vue
@@ -26,17 +26,19 @@
                                 <p class="flex items-center">{{ item.description }}</p>
                             </div>
                             <div class="w-1/3 flex justify-end">
-                                <button class="group relative flex justify-center items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-                                    <router-link to="/test" @click="this.$store.dispatch('setActiveTestById', item.id)"><p class="px-4">Пройти тест</p></router-link>
-                                </button>
+                                <router-link to="/test" @click="this.$store.dispatch('setActiveTestById', item.id)">
+                                    <button class="group relative flex justify-center items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                                        <p class="px-4">Пройти тест</p>
+                                    </button>
+                                </router-link>
                             </div>
                         </div>
                         
                     </div>
                 </div>
-                <div class="justify-center shadow-lg my-3 border-gray-300 border-2 p-[20px] w-3/5 bg-blue-50 rounded-3xl">
+                <div class="justify-center shadow-lg my-3 border-gray-300 border-2 p-[20px] w-full bg-blue-50 rounded-3xl">
                     <div class="flex justify-center my-3">
-                        <input v-model="testId" placeholder="Вставьте код теста" type="text" name="code" id="code" class="w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                        <input v-model="testId" placeholder="Вставьте код теста" type="text" name="code" id="code" class="w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
                         <button @click="addTest" class="text-white mx-10 bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
                             <p class="p-2 font-bold">Добавить тест</p>
                         </button>
@@ -46,20 +48,24 @@
             </div>
         </div>
     </div>
-
 </template>
 
 <script>
 import {mapState} from "vuex";
+// import TestFormStartPage from './TestFormStartPage.vue';
 
 document.title = 'Тесты';
 
 export default {
     name: "tests-list",
+    // components:{
+    //     TestFormStartPage
+    // },
     data(){
         return {
             //tests: ['1', '2'],
             testId: '',
+            isStartPageVisible: false,
         }
     },
     methods:{
--- src/views/TestListView.vue
+++ src/views/TestListView.vue
@@ -2,6 +2,7 @@
     <div class="test-form">
         <HeaderBlock></HeaderBlock>
         <TestList></TestList>
+        <TestFormStartPage></TestFormStartPage>
         <FooterBlock></FooterBlock>
     </div>
 </template>
@@ -11,13 +12,14 @@
 import HeaderBlock from '@/components/HeaderBlock.vue'
 import TestList from '@/components/TestList.vue'
 import FooterBlock from '@/components/FooterBlock.vue'
+import TestFormStartPage from '@/components/TestFormStartPage.vue';
 
 document.title = 'Тесты';
 
 export default {
 name: 'TestListView',
 components: {
-    HeaderBlock, TestList, FooterBlock
+    HeaderBlock, TestList, FooterBlock, TestFormStartPage
 }
 }
 </script>
